18308c93338f032734fc1fc95957b57db88ba38b
Ochuko Benemoh <Bennymoore@Ochukos-MacBook-Air.local>
2014-07-11 18:54:19 -0400

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by Ochuko Benemoh on 2014-07-11.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by Ochuko Benemoh on 2014-07-11.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
